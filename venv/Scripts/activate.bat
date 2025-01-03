b18a5fda666d722d16ce84c1d1a5eef9635f3281
Kiraws <kirawslawson@gmail.com>
2025-01-03 00:00:34 +0000

    ajout de la sidebar et images

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=d:\Docs\Python\Django\Gestion_depenses\venv
+set VIRTUAL_ENV=D:\cours ESAG NDE TIG Lp3\python\Expense-manager\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
